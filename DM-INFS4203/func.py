401b48a259577c6c3b25dc55c4f928f88b4ec6c7
AprilXu8168 <AprilXu8168@gmail.com>
2024-08-11 17:11:02 +1000

    simple codeing tech demo

--- DM-INFS4203/func.py
+++ DM-INFS4203/func.py
@@ -3,9 +3,13 @@ from evaluation import *
 def main():
     record = Record(tp=10, tn=15, fp=5, fn=3)
     prec = record.precision()
+
     print(f"Performance:\nPrecision: {prec:.2f}")
-    
     print("Script running done")
+
+    # result = [10, 15, 5, 3]
+    # prec = result[0]/result[1]+result[0]
+    # print("simgle coding prac", round(prec, 2))
 
 if __name__ == "__main__":
     main()
